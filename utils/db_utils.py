ff840d4218c50f13412bc42c75dbc02a0d6b5bd4
Verrem98 <emielverhoef@hotmail.com>
2021-02-15 14:58:43 +0100

    import fix

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,5 +1,5 @@
 import psycopg2
-from utils import db_auth
+import utils.db_auth
 
 connection = None
 cursor = None
@@ -8,7 +8,7 @@ cursor = None
 def open_db_connection():
     global connection, cursor
     try:
-        connection = db_auth.getPostgreSQLConnection(psycopg2)
+        connection = utils.db_auth.getPostgreSQLConnection(psycopg2)
         cursor = connection.cursor()
     except (Exception, psycopg2.Error) as error:
         print("Error while connecting to PostgreSQL", error)
@@ -27,33 +27,4 @@ def execute_query(query, data):
     cursor.execute(query, data)
     connection.commit()
 
-    close_db_connection()
-
-def fill_products_db(products):
-    open_db_connection()
-    for product in products:
-        try:
-            id = product['_id']
-            brand = product['brand']
-            category = product['category']
-            color = product['color']
-            deeplink = product['deeplink']
-            description = product['description']
-            fast_mover = product['fast_mover']
-            flavor = product['flavor']
-            gender = product['gender']
-            herhaalaankopen = product['herhaalaankopen']
-            name = product['name']
-            predict_out_of_stock_date = product['predict_out_of_stock_date']
-            recommendable = product['recommendable']
-            size = product['size']
-        except:
-            print(product)
-
-        try:
-            cursor.execute("insert into products (id, brand, category, color, deeplink, description, fast_mover, flavor, gender, herhaalaankopen, name, predict_out_of_stock_date, recommendable, size) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-                            "",(id, brand, category, color, deeplink, description,fast_mover, flavor, gender, herhaalaankopen, name, predict_out_of_stock_date, recommendable, size))
-        except:
-            print(product)
-    connection.commit()
     close_db_connection()
