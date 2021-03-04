f4a92c91841db147760930bcf395f7ebca830939
JReesW <j.rees.w@gmail.com>
2021-03-04 14:09:51 +0100

    docstring for get_product_property()
    it's the little things that count

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,4 +1,5 @@
 def get_product_property(product_data, key):
+    """Return the value of a property, returning none if no value is found"""
     try:
         return product_data[key]
     except KeyError:
