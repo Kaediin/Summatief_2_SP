db9bbd02d7c9d4204b5d13f2cdcecd9cd39579c4
kaediin <skaedin@gmail.com>
2021-03-24 17:21:59 +0100

    Testomgeving opzetten #12

--- models/product_prices.py
+++
@@ -1,6 +0,0 @@
-class ProductPrices:
-    def __init__(self, product_id, discount, mrsp, selling_price):
-        self.product_id = product_id
-        self.discount = discount
-        self.mrsp = mrsp
-        self.selling_price = selling_price
