78b40e1950e34a3162785987f66652a24197b045
Verrem98 <emielverhoef@hotmail.com>
2021-03-31 14:10:38 +0200

    made code more readable, split some blocks into functions

--- huw.py
+++ huw.py
@@ -7,7 +7,7 @@ import pprint
 from controller import database_controller as database
 from controller import db_auth
 from model import convert_to_model
-from algorithms import profiles
+from algorithms import profiles, prioritze_discount
 
 # The secret key used for session encryption is randomly generated every time
 # the server is started up. This means all session data (including the 
@@ -377,17 +377,9 @@ class HUWebshop(object):
 
 
 
-    def shoppingcart(self):
-        """ This function renders the shopping cart for the user."""
 
-        i = []
-        limit = 4
-        for tup in session['shopping_cart']:
-            prod_obj = convert_to_model.toProduct(
-                database.retrieve_properties("products", {"product_id": f"{tup[0]}"})[0])
-            product = self.prepproduct(prod_obj)
-            product["itemcount"] = tup[1]
-            i.append(product)
+    def cart_alg_selection(self,limit):
+        "code that decides what algorithm to use in the shopping cart based on the accuracy of the recommendations"
 
         ids_in_cart = [x[0] for x in session['shopping_cart']]
 
@@ -413,9 +405,9 @@ class HUWebshop(object):
                 recs = list(set([product for rec in recs_data if rec[2] >= sample_size_limit for product in rec[1] if
                                  product not in ids_in_cart]))
 
-                recs = self.prioritize_discount(recs,4)
+                recs = prioritze_discount.run(recs,4)
 
-            r_prods = self.convert_to_product_list("select * from products where product_id in %s",(tuple(recs),))
+            r_prods = self.convert_to_product_list("select * from products where product_id in %s", (tuple(recs),))
 
         else:
             try:
@@ -427,6 +419,21 @@ class HUWebshop(object):
             except Exception:
                 r_prods = [convert_to_model.toProduct(e) for e in database.getRandomProducts([], limit)]
 
+        return r_prods
+
+    def shoppingcart(self):
+        """ This function renders the shopping cart for the user."""
+
+        i = []
+        limit = 4
+        for tup in session['shopping_cart']:
+            prod_obj = convert_to_model.toProduct(
+                database.retrieve_properties("products", {"product_id": f"{tup[0]}"})[0])
+            product = self.prepproduct(prod_obj)
+            product["itemcount"] = tup[1]
+            i.append(product)
+
+        r_prods = self.cart_alg_selection(4)
 
         return self.renderpackettemplate('shoppingcart.html', {'itemsincart': i, \
                                                                'r_products': r_prods, \
