504fdc7d440f049bd87f1519fe71bb2aa4953d2f
JReesW <j.rees.w@gmail.com>
2021-04-06 13:16:19 +0200

    Minor fixes, and added comments for #23

--- algorithms/simple.py
+++ algorithms/simple.py
@@ -6,6 +6,8 @@ def run(cursor, connection):
     """
         Drop, then create, the table 'simplerecs', and then fill that table with product ID's and recommended items
     """
+
+    # Check if the table needs to be filled or not
     try:
         cursor.execute("select count(*) from simplerecs")
         hasEntries = True if cursor.fetchone()[0] > 0 else False
@@ -14,6 +16,7 @@ def run(cursor, connection):
         hasEntries = False
 
     if not hasEntries:
+        # Create the table 'simplerecs'
         try:
             cursor.execute("drop table if exists simplerecs")
             cursor.execute("create table simplerecs (product_id varchar primary key, recommendations varchar[] null)")
@@ -25,6 +28,7 @@ def run(cursor, connection):
         cursor.execute("select product_id from product_categories")
         ids = [e[0] for e in cursor.fetchall()]
 
+        # For each product, recommend 4 products and save these in the table 'simplerecs'
         c = 0
         for i in ids:
             i = i.replace("'", "''")
@@ -39,12 +43,15 @@ def recommend(product_id, cursor, limit=4):
     """
         Generate 4 items to recommend based on the given product ID
     """
+
+    # Get the category, sub_category and sub_sub_category associated with the given product ID
     cursor.execute(f"""
                         select p.category, p.sub_category, p.sub_sub_category from product_categories p
                             where p.product_id = '{product_id}'
                     """)
     cat, subcat, subsubcat = [e.replace("'", "''") if e is not None else e for e in cursor.fetchall()[0]]
 
+    # Get all product ID's with the same sub_sub_category, sub_category ca
     cursor.execute(f""" 
                         select t.product_id from (
                             select *, 1 as filter from product_categories pc
@@ -64,10 +71,11 @@ def recommend(product_id, cursor, limit=4):
                             limit {limit}) as t
                     """)
 
+    # If there are not enough products, fill the rest of the list with random products
     res = [str(r[0]) for r in cursor.fetchall()]
-    if len(res) < 4:
+    if len(res) < limit:
         cursor.execute("select product_id from products")
         ids = [e[0] for e in cursor.fetchall()]
 
-        [res.append(e) for e in random.sample(ids, 4) if e not in res and len(res) < 4]
+        [res.append(e) for e in random.sample(ids, limit) if e not in res and len(res) < limit]
     return res
