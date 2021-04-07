5e1f6456b5cd601df6fb8c57ea0f1712b8a1e5aa
hamed <hamedamin98@gmail.com>
2021-04-07 13:03:18 +0200

    Algoritme grotendeels af Te doen: Feestdagen implementatie

--- algorithms/seizoen.py
+++ algorithms/seizoen.py
@@ -2,86 +2,71 @@ import psycopg2
 import datetime
 from collections import Counter
 
+connection = psycopg2.connect(user="postgres",
+                              password="38gAc57ip!",
+                              host="127.0.0.1",
+                              port="5432",
+                              database="postgres")
+mycursor = connection.cursor()
 
-def run(cursor, connection):
-    try:
-        cursor.execute("select count(*) from seizoen_recs")
-        hasEntries = True if cursor.fetchone()[0] > 0 else False
-    except:
-        connection.rollback()
-        hasEntries = False
+#  manually creating the 'seizoens_rec' table with basic SQL
+create_table = "DROP TABLE IF EXISTS seizoens_rec CASCADE;" \
+               "CREATE TABLE seizoens_rec (session_id varchar PRIMARY KEY, datum date, maand integer NULL, seizoen varchar NULL, product varchar[],  recommendation varchar[])"
+mycursor.execute(create_table)
 
-    if not hasEntries:
+#  manually inserting values into the designated columns
+insert_table = "INSERT INTO seizoens_rec (session_id, datum, maand, product)" \
+               "SELECT session_id, date(session_start), EXTRACT(MONTH FROM session_start),  products FROM orders;"
+mycursor.execute(insert_table)
+
+add_seasons_lente = "UPDATE seizoens_rec SET seizoen = 'Lente' WHERE maand BETWEEN 3 AND 5;"
+mycursor.execute(add_seasons_lente)
+
+add_seasons_herfst = "UPDATE seizoens_rec SET seizoen = 'Herfst' WHERE maand BETWEEN 9 AND 11;"
+mycursor.execute(add_seasons_herfst)
+
+add_seasons_zomer = "UPDATE seizoens_rec SET seizoen = 'Zomer' WHERE maand BETWEEN 6 AND 8;"
+mycursor.execute(add_seasons_zomer)
+
+add_seasons_winter = "UPDATE seizoens_rec SET seizoen = 'Winter' WHERE maand IN (12, 1, 2);"
+mycursor.execute(add_seasons_winter)
+
+#  importing the products which where sold during spring season
+mycursor.execute("SELECT product FROM seizoens_rec WHERE seizoen = 'Lente'")
+data_lente = mycursor.fetchall()
+mycursor.execute("SELECT product FROM seizoens_rec WHERE seizoen = 'Herfst'")
+data_herfst = mycursor.fetchall()
+mycursor.execute("SELECT product FROM seizoens_rec WHERE seizoen = 'Zomer'")
+data_zomer = mycursor.fetchall()
+mycursor.execute("SELECT product FROM seizoens_rec WHERE seizoen = 'Winter'")
+data_winter = mycursor.fetchall()
+
+
+def top_four_products(data):  # function that searches for the products with the most occurrences within a specific season.
+    product_counter = {}
+    for product in data:
         try:
-            cursor.execute("drop table if exists seizoen_recs")
-            cursor.execute("create table seizoen_recs (visitor_id varchar primary key, session_date int, recommendations varchar[] null)")
-            print("Table created")
-        except psycopg2.errors.DuplicateTable:
-            connection.rollback()
-            print("Table already exists")
+            p_id = product[0][0]
+            if p_id in product_counter:
+                product_counter[p_id] += 1
+            else:
+                product_counter[p_id] = 1
+        except IndexError:
+            pass
 
-        cursor.execute("select visitor_id from visitors")
+    popular_product = sorted(product_counter, key=product_counter.get, reverse=True)
 
-        ids = [e[0] for e in cursor.fetchall()]
-
-        c = 0
-        for i in ids:
-            i = i.replace("'", "''")
+    top_4 = popular_product[:4]  # You can change the amount of popular products you wish to observe by changing the slice integer
+    return top_4
 
 
+mycursor.execute("UPDATE seizoens_rec SET recommendation = %s WHERE seizoen = 'Lente'", (top_four_products(data_lente),))
 
-'''
+mycursor.execute("UPDATE seizoens_rec SET recommendation = %s WHERE seizoen = 'Herfst'", (top_four_products(data_herfst),))
 
-def session_date():
-    # Lente         = ("Lente", ("2018-03-01","2018-05-31"))
-    # Zomer         = ("Zomer",("2018-06-01","2018-08-31"))
-    # Herfst        = ("Herfst", ("2018-09-01", "2018-11-30"))
-    # Winter        = ("Winter", ("2017-12-01", "2018-02-28"))
-    # Sinterklaas   = ("Sinterklaas", ("2018-11-21", "2018-12-05"))
-    # Kerst         = ("Kerst", ("2018-12-10", "2018-12-25"))
+mycursor.execute("UPDATE seizoens_rec SET recommendation = %s WHERE seizoen = 'Zomer'", (top_four_products(data_zomer),))
 
-    periods = [("Lente", ("2018-03-01", "2018-05-31")), ("Zomer", ("2018-06-01", "2018-08-31")),
-               ("Herfst", ("2018-09-01", "2018-11-30")),
-               ("Winter", ("2017-12-01", "2018-02-28")), ("Sinterklaas", ("2018-11-21", "2018-12-05")),
-               ("Kerst", ("2018-12-10", "2018-12-25"))]
+mycursor.execute("UPDATE seizoens_rec SET recommendation = %s WHERE seizoen = 'Winter'", (top_four_products(data_winter),))
 
-    for i in periods:
-        data = get(i[1][0], i[1][1])
-        data = [item for item, in data]
-        period_name = i[0]
-
-        mostcommon = Counter(data).most_common(10)
-
-        for x in mostcommon:
-            insert_into_postgres("most_bought_period", (x[0], "'" + period_name + "'"))
-
-
-time_periods()
-
-
-
-def most_bought_daily():
-    #   Je geeft 2 data op van de dagen waartussen je de records wil krijgen
-    #   Wil je de records van 1 dag, bijvoorbeeld 2017-12-10 dan doe je get('2017-12-10', '2017-12-11').
-
-    data = get('2017-12-18', '2017-12-19')
-    dag = ("2017-12-18")
-    data = [item for item, in data]
-    print(data)
-
-    #   Dit werkt niet, maar als de op=op voordeelshop nog echt zou opereren zou dit gebruikt kunnen worden
-    #   In plaats van handmatig de dag in te voeren
-    # dag_ = datetime.date.today()
-    # dag_2 = datetime.date.today() + datetime.timedelta(days=1)
-    # data_ = get(dag_, dag_2)
-    # data_ = [item for item, in data_]
-
-    mostcommon = Counter(data).most_common(3)
-    print(mostcommon)
-    for x in mostcommon:
-        insert_into_postgres("most_bought_day", (x[0], "'" + dag + "'"))
-
-
-most_bought_daily()
-
-'''
+mycursor.close()
+connection.commit()
